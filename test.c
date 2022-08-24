adbb1922c5f482498be267047f646e02034464af
Susmita-Goswami <108253713+Susmita-Goswami@users.noreply.github.com>
2022-08-24 18:13:57 +0600

    Add files via upload

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int i;
+    float x;
+    double z;
+    char c;
+    printf("Size of int =%d bytes\n",sizeof(i));
+    printf("Size of float =%d bytes\n",sizeof(x));
+    printf("Size of double =%d bytes\n",sizeof(z));
+    printf("Size of char =%d bytes\n",sizeof(c));
+
+}
